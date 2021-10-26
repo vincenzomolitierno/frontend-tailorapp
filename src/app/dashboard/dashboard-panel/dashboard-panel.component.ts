925d116d2f8e61c42f05256559f06139b1bebc9b
Vincenzo <vincenzo.molitierno@gmail.com>
2021-10-26 12:05:10 +0200

    commit

--- src/app/dashboard/dashboard-panel/dashboard-panel.component.ts
+++ src/app/dashboard/dashboard-panel/dashboard-panel.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/authentication/authentication.service';
 import { ScriptService } from 'src/app/customers/customer-grid/script.service';
+import server from "src/assets/setting/server.json";
+import app_setting from "src/assets/setting/app-setting.json";
 
 @Component({
   selector: 'app-dashboard-panel',
@@ -10,12 +12,17 @@ import { ScriptService } from 'src/app/customers/customer-grid/script.service';
 })
 export class DashboardPanelComponent implements OnInit {
 
+  private server;
+  private app_setting
+
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService,
     public scriptService: ScriptService
   ) { 
     this.scriptService.load('pdfMake', 'vfsFonts');
+    this.server = server;
+    this.app_setting = app_setting;
   }
 
   ngOnInit() {
